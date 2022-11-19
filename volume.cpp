d1e263892194892b1d046c3f1f917310342f7af7
Rahul Singh Bhadoriya <118618909+bhadoriyarahul@users.noreply.github.com>
2022-11-19 21:05:37 +0530

    Add files via upload

---
+++ volume.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int v,l,b,h,i;
+    cout<<"value of length:";
+    cin>>l;
+    cout<<"value of breath:";
+    cin>>b;
+    cout<<"value of height:";
+    cin>>h;
+    v=l*b*h;
+    cout<<"total volume="<<v<<endl;
+    return 0;
+}
+
+
